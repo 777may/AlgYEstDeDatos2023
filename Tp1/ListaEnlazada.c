18ba8b92221925d65fb011ac309797f4af3dd40e
777may <mayri.luli@gmail.com>
2023-03-20 18:34:15 -0300

    ahora a ir a TdeLenguaje

--- Tp1/ListaEnlazada.c
+++ Tp1/ListaEnlazada.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<ctype.h>
-
 //    1
     typedef int tipo;
 
     struct Nodo{
         tipo dato;
-        struct Nodo* siguiente;
+        LISTA* siguiente;
     }; 
     typedef struct Nodo* LISTA;
 
@@ -15,13 +13,24 @@
 LISTA crearLista();
 int esListaVacia(LISTA L);
 void mostrar(LISTA L);
+void primerElemento(LISTA L);
+LISTA insertar(LISTA L, tipo datoNodo);
+LISTA borrar(LISTA L);
+
+
 /////////
 void main(){
 
-    LISTA A = crearLista();
+    // LISTA A = crearLista();
     
-    printf("%d", esListaVacia(A));
+    // printf("%d\n", esListaVacia(A));
+    // mostrar(A);
 
+    // LISTA B;
+    // A = &B;
+    // A->dato = 13;
+    // A->siguiente = crearLista();
+    // mostrar(A);
 }
 /////////
 // // 2
@@ -38,18 +47,52 @@ void mostrar(LISTA L){
     if (!esListaVacia(L)){
         while (!esListaVacia(L)){
             if (sizeof(tipo) == sizeof(int)){
-                printf("%d", L->dato);
+                printf("%d \n", L->dato);
             } else if (sizeof(tipo) == sizeof(char))
             {
-                printf("%s", L->dato);
+                printf("%s \n", L->dato);
             }
             L = L->siguiente;
         }
     }else{
-        printf("La lista está vacia");
+        printf("La lista está vacia\n");
     };
 }
 // // // D
+void primerElemento(LISTA L){
+    if (!esListaVacia(L)){
+        if (sizeof(tipo) == sizeof(int)){
+            printf("%d \n", L->dato);
+        } else if (sizeof(tipo) == sizeof(char))
+        {
+            printf("%s \n", L->dato);
+        }
+    }else{
+        printf("La lista está vacia\n");
+    };
+}
 // // // E
+LISTA insertar(LISTA L, tipo datoNodo){
+    LISTA Nuevo;
+    Nuevo = malloc(sizeof(LISTA));
+    Nuevo->dato = datoNodo;
+    Nuevo->siguiente = L; 
+    L = Nuevo;
+    return L;
+}
+
 // // // F
+LISTA borrar(LISTA L){
+    if (!esListaVacia(L)){
+        LISTA aux;
+        aux = L;
+        L = L->siguiente;
+        free(aux);
+    }else{
+        printf("La lista está vacia\n");
+    };
+}
 // // // G
+
+// // // H
+// // // I
